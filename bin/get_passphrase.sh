21c498fef333e0ba5ad9b4dddff1c8789eca3f0e
Luca Guzzon <luca.guzzon@gmail.com>
2024-08-26 10:40:56 +0000

    fix(get_passphrase): 🔧 - Improve passphrase handling
    - Changed the syntax for checking the WARD_PASSPHRASE environment variable to use
      parameter expansion.
    - Updated the echo command to use double quotes for consistency.
    - Simplified the prompt for entering the passphrase using the `-p` option with `read`.
    - Ensured consistent indentation and formatting throughout the script.

--- bin/get_passphrase.sh
+++ bin/get_passphrase.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Check if the passphrase is stored in an environment variable
-if [ -n "$WARD_PASSPHRASE" ]; then
-    echo "$WARD_PASSPHRASE"
-    exit 0
+if [ -n "${WARD_PASSPHRASE-}" ]; then
+  echo "${WARD_PASSPHRASE}"
+  exit 0
 fi
 
 # If not, and we're in an interactive environment, prompt the user
 if [ -t 0 ]; then
-    echo -n "Enter the passphrase for encryption: " >&2
-    read -s passphrase
-    echo >&2
-    echo "$passphrase"
+  read -r -s -p "Enter the passphrase for encryption: " passphrase
+  echo
+  echo "${passphrase}"
 else
-    echo "Error: WARD_PASSPHRASE environment variable not set in non-interactive mode." >&2
-    exit 1
+  echo "Error: WARD_PASSPHRASE environment variable not set in non-interactive mode." >&2
+  exit 1
 fi
 
